a384aaba155755def314a8397e08cd951dcc4ca3
adheeba <adeebathahsin@gmail.com>
2025-03-15 14:44:10 +0100

    final

--- recommendation.py
+++ recommendation.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from PIL import Image
 st.set_page_config(
-    page_title="'AI-Powered Employee Recommendation System",
+    page_title="Employee Recommendation System",
     page_icon="🚀",
     layout="wide",
     initial_sidebar_state="expanded"
@@ -12,7 +13,7 @@ st.markdown("""
     <style>
 .card {
     border: 2px solid #007bff; /* Blue border */
-    background-color: #f8f9fa; /* Light gray background */
+    background-color: #21C0FA; /* Light gray background */
     padding: 15px;
     border-radius: 8px; /* Rounded corners */
     box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1); /* Light shadow */
@@ -21,6 +22,15 @@ st.markdown("""
     }
 </style>
 """, unsafe_allow_html=True)
+with st.sidebar:
+    st.image("logo.jpeg", use_container_width=False)
+    st.markdown("""
+    ## About the tool
+    AI Powered ERS is a Strategic Skill Planner tool which helps Work force planners
+                in selecting the best employees from his company given the skill set needed.
+    """)
+    #st.button(":blue[Get Skills]")
+    #st.button(":blue[Recommend Employees]")
 # Load dataset
 @st.cache_data
 def load_data():
@@ -32,18 +42,24 @@ df = load_data()
 vectorizer = TfidfVectorizer()
 tfidf_matrix = vectorizer.fit_transform(df['Skills'])
 
+
 # Streamlit UI
 image, title = st.columns([0.8,2])
+with image:
+  image = Image.open('hey_logo.jpg')
+  st.image(image, width=300)
 with title:
-    st.title("🚀 :blue[_AI-Powered_ _Employee_ _Recommendation_ _System_]")
-    st.markdown("Find the best employees based on your required skills!")
+    st.title("🚀 :blue[AI-Powered ERS]")
+    st.markdown("# Find the best employees based on your required skills!")
 import csv
 
 
 col11,col21,col31 = st.columns([0.5,1,0.3])
 # User input for skills
 with col21:
-    user_input = st.text_input("Enter skills (comma-separated):", "")
+    #user_input = st.text_input("## Enter skills (comma-separated):", "")
+    st.markdown("## Enter skills (comma-separated):")
+    user_input = st.text_input("", "")
 
 if user_input:
     # Convert user input into vector
